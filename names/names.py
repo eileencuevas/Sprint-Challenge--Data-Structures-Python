cf2da7f4c4bd78b08274e2eeb1b83b0885257196
steelworx <eileencuevas62@gmail.com>
2019-04-05 12:55:10 -0400

    added first attempt at stretch for names.py

--- names/names.py
+++ names/names.py
@@ -10,14 +10,23 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# ---------- Original code for names.py ---------- #
+
 # duplicates = []
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
+
+# --------------- MVP for names.py --------------- #
 names_dict = {name: 1 for name in names_1}
 duplicates = [name for name in names_2 if name in names_dict]
+
+
+# ------------- Stretch for names.py ------------- #
+# duplicates = [
+#     name_1 for name_1 in names_1 for name_2 in names_2 if name_1 == name_2]
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
