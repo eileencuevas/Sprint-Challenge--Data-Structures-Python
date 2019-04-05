8d996d4a28d383d391406933a3139387500765c4
steelworx <eileencuevas62@gmail.com>
2019-04-05 10:59:17 -0400

    initial commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,11 @@
-class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+class RingBuffer:  # this is basically a FIFO queue
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        pass
 
-  def get(self):
-    pass
+    def get(self):
+        pass
