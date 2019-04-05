5cf009a0c395bd5586c1febf52b4a12c85d28ee3
steelworx <eileencuevas62@gmail.com>
2019-04-05 11:12:50 -0400

    completed ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,15 @@
-class RingBuffer:  # this is basically a FIFO queue
+class RingBuffer:  # this is similar to a FIFO queue
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = 0
-        self.storage = [None]*capacity
+        self.current = 0  # this is the current index in the storage to overwrite
+        self.storage = [None]*capacity  # storage preset to be a certain length
 
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        if self.current < self.capacity - 1:
+            self.current += 1
+        else:
+            self.current = 0
 
     def get(self):
-        pass
+        return [item for item in self.storage if item is not None]
